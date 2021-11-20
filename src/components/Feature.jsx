cd160af2a5939b709fd1b43e05bf113f35fe9aa6
Jose Otniel <joseotniel2010@gmail.com>
2021-11-20 13:20:20 -0500

    Fixed typo in clases

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -4,12 +4,12 @@ import './feature.css'
 
 const Feature = ({ title, text }) => {
 	return (
-		<div className="gpt3__feature-container__feature">
-			<div className="gpt3__feature-container__feature-title">
+		<div className="gpt3__features-container__feature">
+			<div className="gpt3__features-container__feature-title">
 				<div />
 				<h1>{title}</h1>
 			</div>
-			<div className="gpt3__feature-container__feature-text">
+			<div className="gpt3__features-container__feature-text">
 				<p>{text}</p>
 			</div>
 		</div>
