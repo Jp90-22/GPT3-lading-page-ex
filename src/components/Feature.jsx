cb09cefa757a135526c6209cf33207780fe4d7e0
Jose Otniel <joseotniel2010@gmail.com>
2021-11-20 13:56:39 -0500

    Implemented Feature and WhatGPT3 components, adjusted WhatGPT3's style just a little and changed a bad selection

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -9,7 +9,7 @@ const Feature = ({ title, text }) => {
 				<div />
 				<h1>{title}</h1>
 			</div>
-			<div className="gpt3__features-container__feature-text">
+			<div className="gpt3__features-container_feature-text">
 				<p>{text}</p>
 			</div>
 		</div>
