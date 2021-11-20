2f18e9a2d5a5bfac252298ca541e2e0d8d3883b8
Jose Otniel <joseotniel2010@gmail.com>
2021-11-20 12:58:43 -0500

    Implemented Feature component, now working on its style and finishing WhatGPT3 component

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
 	return (
-		<div>
-			Feature component
+		<div className="gpt3__feature-container__feature">
+			<div className="gpt3__feature-container__feature-title">
+				<div />
+				<h1>{title}</h1>
+			</div>
+			<div className="gpt3__feature-container__feature-text">
+				<p>{text}</p>
+			</div>
 		</div>
 	)
 }
