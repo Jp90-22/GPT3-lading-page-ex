59cf2d35619193ba9b59f460db732bc6acdd5b13
Jose Otniel <joseotniel2010@gmail.com>
2021-11-23 12:26:55 -0500

    Implemented Article and Blog components

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 import './article.css'
 
-const Article = () => {
+const Article = ({ imgUrl, date, title }) => {
 	return (
-		<div>
-			Article compoent
+		<div className="gpt3__blog-container_article">
+			<div className="gpt3__blog-container_article-image">
+				<img src={imgUrl} alt="About the blog" />
+			</div>
+			<div className="gpt3__blog-container_article-content">
+				<div>
+					<p>{date}</p>
+					<h3>{title}</h3>
+				</div>
+				<p>Read full article >></p>
+			</div>
 		</div>
 	)
 }
