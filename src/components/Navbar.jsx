e2ad753f052c684d49bfe41b300e578681f00125
Jose Otniel <joseotniel2010@gmail.com>
2021-11-24 16:09:17 -0500

    Changed Navbar structure and implemented Footer component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,8 @@ const Menu = () => (
 	<>
 		<p><a href="#home">Home</a></p>
 		<p><a href="#wgpt3">WhatGPT3</a></p>
-		<p><a href="#possibility">Open AI</a></p>
 		<p><a href="#features">Case Studies</a></p>
+		<p><a href="#possibility">Open AI</a></p>
 		<p><a href="#blog">Library</a></p>
 	</>
 )
