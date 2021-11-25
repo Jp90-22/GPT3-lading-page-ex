e8fcecc26f62f8b3672ca2a36ea79e05a40eda68
Jose Otniel <joseotniel2010@gmail.com>
2021-11-25 14:02:30 -0500

    Implemented the functionality of Alert component

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Alert from "../components/Alert"
 
 import './header.css'
@@ -6,9 +6,29 @@ import people from '../assets/people.png'
 import ai from '../assets/ai.png'
 
 const Header = () => {
+	const [isAlertShowing, setIsAlertShowing] = useState(false)
+	const [email, setEmail] = useState("")
+
+	const validateEmail = (email) => email.match(
+    	/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  	)
+
+	const onEmailInputChanged = (evt) => setEmail(evt.target.value)
+
+	const onEmailBtnClicked = () => {
+		if (email && validateEmail(email)) {
+			setIsAlertShowing(true)
+
+			// Starts a time out for quit alert after 10 seconds
+			setTimeout(() => setIsAlertShowing(false), 10000);
+		} else {
+			setEmail("Bad email!")
+		}
+	}
+
 	return (
 		<>
-			<Alert />
+			<Alert show={isAlertShowing} handler={() => setIsAlertShowing(false)} />
 			<div className="gpt3__header section__padding" id="home">
 				<div className="gpt3__header-content">
 					<h1 className="gradient__text">
@@ -19,8 +39,8 @@ const Header = () => {
 					</p>
 
 					<div className="gpt3__header-content__input">
-						<input type="email" placeholder="Your Email Address" />
-						<button type="button">Get Started</button>
+						<input type="email" placeholder="Your Email Address" value={email} onChange={onEmailInputChanged} />
+						<button type="button" onClick={onEmailBtnClicked}>Get Started</button>
 					</div>
 
 					<div className="gpt3__header-content__people">
