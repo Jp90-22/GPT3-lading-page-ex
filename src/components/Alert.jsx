01f71b131b67f8283990087a8eabb39b89c0d891
Jose Otniel <joseotniel2010@gmail.com>
2021-11-25 13:53:23 -0500

    Fixed Alert styling and putted it an animation

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 
 import './alert.css'
 
-const Alert = () => {
-	return (
-		<div className="gpt3__alert-container" id="alert">
-			<div className="gpt3__alert-content">
-				<h2>You're registered now!!</h2>
-				<p>
-					You took your first step in to the future, check out our <a href="#blog">library</a> to get started.
-				</p>
+const Alert = ({ show, handler }) => {
+	
+	if (show) {
+		return (
+			<div className="gpt3__alert-container" id="alert">
+				<div className="gpt3__alert-content scale-up-center" onClick={handler}>
+					<h2>You're registered now!!</h2>
+					<p>
+						You took your first step in to the future, check out our <a href="#blog">library</a> to get started.
+					</p>
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
+
+	return null
 }
 
 export default Alert
